78a63c142face2580835a55c2af3ef535d44920f
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2023-12-01 22:28:52 +0800

    [useCompany] add addCompany

--- src/hooks/useCompany.ts
+++ src/hooks/useCompany.ts
@@ -1,11 +1,30 @@
+import { ICompany } from "@/types/company/ICompany";
 import { axiosPrivate } from "@/utils/axios";
 
 export function useCompany() {
-  const getAllCompanyHelper = async () => {
+  const getAllCompany = async () => {
     const response = await axiosPrivate.get("company/");
     const companies = response.data.data[0];
     console.log(companies);
     return companies;
   };
-  return getAllCompanyHelper;
+  const addCompany = async (
+    company_name: string,
+    username: string,
+    email: string,
+    wallet_address: string
+  ) => {
+    try {
+      const response = await axiosPrivate.post("company/", {
+        owner: wallet_address,
+        company_name,
+        username,
+        email,
+      });
+      return response.data as ICompany;
+    } catch (err) {
+      throw err;
+    }
+  };
+  return { getAllCompany, addCompany };
 }
