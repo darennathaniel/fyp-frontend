6c1c936f75d8d7aa7f5c9e92d8e98837798a968a
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2023-12-04 11:05:44 +0800

    [ProfileHome] create a profile home page integrate with backend

--- src/app/Profile/Home/index.tsx
+++ src/app/Profile/Home/index.tsx
@@ -1,3 +1,81 @@
+import ProfileHeader from "../components/ProfileHeader";
+import { Separator } from "@/components/ui/separator";
+import { useEffect, useState } from "react";
+import { useUser } from "@/hooks/useUser";
+import { IUserInfo } from "@/types/user/IUserInfo";
+import { useError } from "@/hooks/useError";
+import { useLoading } from "@/hooks/useLoading";
+import { AxiosError } from "axios";
+
 export default function ProfileHome() {
-  return <div>Home</div>;
+  const { getUserInfo } = useUser();
+  const { showError } = useError();
+  const { showLoading, closeLoading } = useLoading();
+  const [user, setUser] = useState<IUserInfo>({
+    username: "",
+    company_name: "",
+    email: "",
+    wallet_address: "",
+    upstream: 0,
+    downstream: 0,
+    supply: 0,
+    prerequisite: 0,
+    isAuthenticated: false,
+  });
+  useEffect(() => {
+    showLoading();
+    getUserInfo()
+      .then((user) => {
+        setUser(user);
+      })
+      .catch((err) => {
+        if (err instanceof AxiosError) {
+          showError(err);
+        }
+      })
+      .finally(() => closeLoading());
+  }, []);
+  return (
+    <div className="space-y-6">
+      <ProfileHeader
+        title="Profile"
+        subtitle="This is how others will see you on the site."
+      />
+      <div className="flex">
+        <div className="font-semibold w-1/2">Company Name</div>
+        <div className="text-zinc-400 w-1/2">{user.company_name}</div>
+      </div>
+      <div className="flex">
+        <div className="font-semibold w-1/2">Username</div>
+        <div className="text-zinc-400 w-1/2">{user.username}</div>
+      </div>
+      <div className="flex">
+        <div className="font-semibold w-1/2">Email</div>
+        <div className="text-zinc-400 w-1/2">{user.email}</div>
+      </div>
+      <div className="flex">
+        <div className="font-semibold w-1/2">Wallet Address</div>
+        <div className="text-zinc-400 w-1/2">{user.wallet_address}</div>
+      </div>
+      <Separator className="bg-white" />
+      <div className="flex">
+        <div className="font-semibold w-1/2">Number of Upstream Company</div>
+        <div className="text-zinc-400 w-1/2">{user.upstream}</div>
+      </div>
+      <div className="flex">
+        <div className="font-semibold w-1/2">Number of Downstream Company</div>
+        <div className="text-zinc-400 w-1/2">{user.downstream}</div>
+      </div>
+      <div className="flex">
+        <div className="font-semibold w-1/2">Number of Product Supply</div>
+        <div className="text-zinc-400 w-1/2">{user.supply}</div>
+      </div>
+      <div className="flex">
+        <div className="font-semibold w-1/2">
+          Number of Prerequisite Product Supply
+        </div>
+        <div className="text-zinc-400 w-1/2">{user.prerequisite}</div>
+      </div>
+    </div>
+  );
 }
--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,14 +1,19 @@
 import { login } from "@/reducers/app";
 import { IUser } from "@/types/user/IUser";
+import { IUserInfo } from "@/types/user/IUserInfo";
 import { axiosPrivate } from "@/utils/axios";
 import { useAppDispatch } from "./useAppDispatch";
 
 export function useUser() {
   const dispatch = useAppDispatch();
-  const userHelper = async () => {
+  const getUser = async () => {
     const response = await axiosPrivate.get("user/");
     const user = response.data.data[0] as IUser;
     dispatch(login({ ...user, isAuthenticated: true }));
   };
-  return userHelper;
+  const getUserInfo = async () => {
+    const response = await axiosPrivate.get("user/info");
+    return { ...response.data.data[0], isAuthenticated: true } as IUserInfo;
+  };
+  return { getUser, getUserInfo };
 }
