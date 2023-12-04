d773e430437e652abd9a18756ffae1849c22d464
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2023-12-04 11:14:03 +0800

    [SidebarApp] add title and subtitle props

--- src/app/Profile/index.tsx
+++ src/app/Profile/index.tsx
@@ -5,7 +5,15 @@ import { Routes, Route } from "react-router-dom";
 export default function Profile() {
   return (
     <Routes>
-      <Route element={<SidebarApp sidebarNavItems={profileRoutes} />}>
+      <Route
+        element={
+          <SidebarApp
+            title="Account"
+            subtitle="Manage your account, contract, and requests."
+            sidebarNavItems={profileRoutes}
+          />
+        }
+      >
         {profileRoutes.map((route) => (
           <Route
             path={route.href.split("/")[route.href.split("/").length - 1]}
